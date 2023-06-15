e271ba6f603758c67296369324fd726490a92e2d
HakamQadi <hakam.h.qadi@gmail.com>
2023-06-15 20:24:45 +0300

    init done

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/*!!!! DO NOT CALL HERE ANY THING BUT THE App.js !!!! */}
     <App />
   </React.StrictMode>
 );
