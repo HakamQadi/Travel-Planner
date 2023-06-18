1994c537a547d9cc1d89405e8d5c41b708e957aa
mohammed saeed <mohammed.f.zain@gmail.com>
2023-06-18 10:21:43 +0300

    routes done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import './App.css';
-import Transfers from './components/transfers/Transfers';
+import "./App.css";
+import Cart from "./components/cart/Cart";
+import Home from "./components/home/Home";
+import Hotels from "./components/hotels/Hotels";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Resturants from "./components/resturants/Resturants";
+import Tours from "./components/tours/Tours";
+import Transfers from "./components/transfers/Transfers";
 
 function App() {
   return (
-    // call the components here 
+    // call the components here
     <div className="App">
-      
-      <Transfers/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/hotels" element={<Hotels />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/resturants" element={<Resturants />} />
+          <Route path="/tours" element={<Tours />} />
+          <Route path="/transfers" element={<Transfers />} />
+        </Routes>
+      </Router>
     </div>
-
   );
 }
 
---
+++ src/components/hotels/Hotels.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hotels() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Hotels
