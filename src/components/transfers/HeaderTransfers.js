f99828a2a09f63c6f08e6bf041253d3676166aea
abdallah <abdallah.edeen@gmail.com>
2023-06-19 09:29:55 +0300

    abd

--- src/components/transfers/HeaderTransfers.js
+++ src/components/transfers/HeaderTransfers.js
@@ -8,97 +8,7 @@ export default function HeaderTransfers() {
   return (
     <div>
       <div className="containerHT">
-        <img
-          src={require("./imgTransfers/transfer_top.jpg")}
-          style={{ height: "500px", width: "100%" }}
-          className="imgHead"
-        />
-
-        <div className="pa">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="10"
-            fill="currentColor"
-            class="bi bi-telephone"
-            viewBox="0 0 16 16"
-          >
-            <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
-          </svg>
-          <strong>0045 043204434</strong>
-        </div>
-
-        <div className="top_links">
-          <ul>
-            <li>
-              <a href="#sign-in-dialog" id="access_link">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-box-arrow-in-right"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"
-                  />
-                  <path
-                    fill-rule="evenodd"
-                    d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
-                  />
-                </svg>
-                Sign in |
-              </a>
-            </li>
-            <li>
-              <a href="wishlist.html" id="wishlist_link">
-                
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-suit-heart"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z" />
-                  </svg>
-                
-                Wishlist |
-              </a>
-            </li>
-            <li>
-              <a href="https://1.envato.market/ryzjQ" target="_parent">
-                Purchase this template
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="line">
-      <hr></hr>
-        </div>
-
-        <div className='LogoT'>
-          <img src={require('./imgTransfers/logo .png')} />
-        </div>
-
-        <div className='menuTransfers'>
-          <ul>
-            <li><a href='#'> Home </a></li>
-
-            <li><a href='#'> Tours </a></li>
-
-            <li><a href='#'> Transfers </a></li>
-
-            <li><a href='#'> Restaurants </a></li>
-
-
-          </ul>
-
-        </div>
+    
 
         <div className='titelTransfers'>
           <h1>PARIS TRANSFER</h1>
@@ -106,6 +16,8 @@ export default function HeaderTransfers() {
         </div>
       </div>
 
+
+      {/* <---------------start Map ----------> */}
       <div id="position">
 			<div class="containerMap">
 				<ul>
@@ -117,6 +29,7 @@ export default function HeaderTransfers() {
 				</ul>
 			</div>
 		</div>
+     {/* <---------------End Map ----------> */}
     </div>
   );
 }
