d9dc0b5db14df17bc460505ea793fd14acb3646a
mohammed saeed <mohammed.f.zain@gmail.com>
2023-06-21 15:55:30 +0300

    finally the blur done

--- src/components/reservation/ReservationForm.js
+++ src/components/reservation/ReservationForm.js
@@ -75,7 +75,52 @@ function ReservationForm() {
 
   return (
     <>
-      <div className={`reservation-form ${blurPage ? "blur" : ""}`}>
+      <div className={`reservation-page ${blurPage ? "blur" : ""}`}>
+        <div className="second-page-heading">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <h4>Book Prefered Deal Here</h4>
+                <h2>Make Your Reservation</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt uttersi labore et dolore magna
+                  aliqua is ipsum suspendisse ultrices gravida
+                </p>
+                <div className="main-button">
+                  <a href="about.html">Discover More</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="more-info reservation-info">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-4 col-sm-6">
+                <div className="info-item">
+                  <i className="fa fa-phone"></i>
+                  <h4>Make a Phone Call</h4>
+                  <a href="/">+123 456 789 (0)</a>
+                </div>
+              </div>
+              <div className="col-lg-4 col-sm-6">
+                <div className="info-item">
+                  <i className="fa fa-envelope"></i>
+                  <h4>Contact Us via Email</h4>
+                  <a href="/">company@email.com</a>
+                </div>
+              </div>
+              <div className="col-lg-4 col-sm-6">
+                <div className="info-item">
+                  <i className="fa fa-map-marker"></i>
+                  <h4>Visit Our Offices</h4>
+                  <a href="/">24th Street North Avenue London, UK</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="reservation-form">
           <div className="container">
             <div className="row">
@@ -83,7 +128,7 @@ function ReservationForm() {
                 <div id="map">
                   <iframe
                     title="any"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12469.776493332698!2d-80.14036379941481!3d25.907788681148624!2m3!1f357.26927939317244!2f20.870722720054623!3f0!3m2!1i1024!2i768!4f35!3m3!1m2!1s0x88d9add4b4ac788f%3A0xe77469d09480fcdb!2sSunny%20Isles%20Beach!5e1!3m2!1sen!2sth!4v1642869952544!5m2!1sen!2sth"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13541.182439619848!2d35.92971506183624!3d31.952876190921465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151b5f86956dc02d%3A0xa9edbdbd70c4d41c!2z2KzYqNmEINi52YXYp9mG2Iwg2LnZhdmR2KfZhg!5e0!3m2!1sar!2sjo!4v1687349991175!5m2!1sar!2sjo"
                     width="100%"
                     height="450px"
                     frameBorder="0"
@@ -97,173 +142,167 @@ function ReservationForm() {
                 </div>
               </div>
               <div className="col-lg-12">
-                <form
-                  style={{
-                    position: 'relative',
-                  }}
-                  id="reservation-form"
-                  name="gs"
-                  method="submit"
-                  role="search"
-                  action="#"
-                  onSubmit={validateForm}
-                >
-                  <div className="row">
-                    <div className="col-lg-12">
-                      <h4>
-                        Make Your <em>Reservation</em> Through This{" "}
-                        <em>Form</em>
-                      </h4>
+                <div className={`reservation-form ${blurPage ? "blur" : ""}`}>
+                  <form
+                    style={{
+                      position: "relative",
+                    }}
+                    id="reservation-form"
+                    name="gs"
+                    method="submit"
+                    role="search"
+                    action="#"
+                    onSubmit={validateForm}
+                  >
+                    <div className="row">
+                      <div className="col-lg-12">
+                        <h4>
+                          Make Your <em>Reservation</em> Through This{" "}
+                          <em>Form</em>
+                        </h4>
+                      </div>
+                      <div className="col-lg-6">
+                        <fieldset>
+                          <label htmlFor="Name" className="form-label">
+                            Your Name
+                          </label>
+                          <input
+                            type="text"
+                            name="Name"
+                            className="Name"
+                            placeholder="Enter Your Name ..."
+                            autoComplete="on"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                          />
+                          {nameError && (
+                            <span className="error" style={{ color: "red" }}>
+                              {nameError}
+                            </span>
+                          )}
+                        </fieldset>
+                      </div>
+                      <div className="col-lg-6">
+                        <fieldset>
+                          <label htmlFor="Number" className="form-label">
+                            Your Phone Number
+                          </label>
+                          <input
+                            type="text"
+                            name="Number"
+                            className="Number"
+                            placeholder="Ex. +xxx xxx xxx"
+                            autoComplete="on"
+                            value={number}
+                            onChange={(e) => setNumber(e.target.value)}
+                          />
+                          {numberError && (
+                            <span className="error" style={{ color: "red" }}>
+                              {numberError}
+                            </span>
+                          )}
+                        </fieldset>
+                      </div>
+                      <div className="col-lg-6">
+                        <fieldset>
+                          <label htmlFor="chooseGuests" className="form-label">
+                            Number Of Guests
+                          </label>
+                          <select
+                            name="Guests"
+                            className="form-select"
+                            aria-label="Default select example"
+                            id="chooseGuests"
+                            required
+                            value={guests}
+                            onChange={(e) => setGuests(e.target.value)}
+                          >
+                            <option value="">How Many Guests</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4+">4+</option>
+                          </select>
+                        </fieldset>
+                      </div>
+                      <div className="col-lg-6">
+                        <fieldset>
+                          <label htmlFor="Number" className="form-label">
+                            Check In Date
+                          </label>
+                          <input
+                            type="date"
+                            name="date"
+                            className="date"
+                            required
+                            value={checkInDate}
+                            onChange={(e) => setCheckInDate(e.target.value)}
+                          />
+                        </fieldset>
+                      </div>
+                      <div className="col-lg-12">
+                        <fieldset>
+                          <label
+                            htmlFor="chooseDestination"
+                            className="form-label"
+                          >
+                            Choose Your Destination
+                          </label>
+                          <select
+                            name="Destination"
+                            className="form-select"
+                            aria-label="Default select example"
+                            id="chooseCategory"
+                            required
+                            value={destination}
+                            onChange={(e) => setDestination(e.target.value)}
+                          >
+                            <option value="">Select Destination</option>
+                            {tours.map((tour) => (
+                              <option key={tour.id} value={tour.name}>
+                                {tour.name}
+                              </option>
+                            ))}
+                          </select>
+                        </fieldset>
+                      </div>
+                      <div className="col-lg-12">
+                        <fieldset>
+                          <button
+                            type="submit"
+                            id="form-submit"
+                            className="main-button"
+                          >
+                            Make Reservation
+                          </button>
+                        </fieldset>
+                      </div>
                     </div>
-                    <div className="col-lg-6">
-                      <fieldset>
-                        <label htmlFor="Name" className="form-label">
-                          Your Name
-                        </label>
-                        <input
-                          type="text"
-                          name="Name"
-                          className="Name"
-                          placeholder="Enter Your Name ..."
-                          autoComplete="on"
-                          value={name}
-                          onChange={(e) => setName(e.target.value)}
-                        />
-                        {nameError && (
-                          <span className="error" style={{ color: "red" }}>
-                            {nameError}
-                          </span>
-                        )}
-                      </fieldset>
-                    </div>
-                    <div className="col-lg-6">
-                      <fieldset>
-                        <label htmlFor="Number" className="form-label">
-                          Your Phone Number
-                        </label>
-                        <input
-                          type="text"
-                          name="Number"
-                          className="Number"
-                          placeholder="Ex. +xxx xxx xxx"
-                          autoComplete="on"
-                          value={number}
-                          onChange={(e) => setNumber(e.target.value)}
-                        />
-                        {numberError && (
-                          <span className="error" style={{ color: "red" }}>
-                            {numberError}
-                          </span>
-                        )}
-                      </fieldset>
-                    </div>
-                    <div className="col-lg-6">
-                      <fieldset>
-                        <label htmlFor="chooseGuests" className="form-label">
-                          Number Of Guests
-                        </label>
-                        <select
-                          name="Guests"
-                          className="form-select"
-                          aria-label="Default select example"
-                          id="chooseGuests"
-                          required
-                          value={guests}
-                          onChange={(e) => setGuests(e.target.value)}
-                        >
-                          <option value="">How Many Guests</option>
-                          <option value="1">1</option>
-                          <option value="2">2</option>
-                          <option value="3">3</option>
-                          <option value="4+">4+</option>
-                        </select>
-                      </fieldset>
-                    </div>
-                    <div className="col-lg-6">
-                      <fieldset>
-                        <label htmlFor="Number" className="form-label">
-                          Check In Date
-                        </label>
-                        <input
-                          type="date"
-                          name="date"
-                          className="date"
-                          required
-                          value={checkInDate}
-                          onChange={(e) => setCheckInDate(e.target.value)}
-                        />
-                      </fieldset>
-                    </div>
-                    <div className="col-lg-12">
-                      <fieldset>
-                        <label
-                          htmlFor="chooseDestination"
-                          className="form-label"
-                        >
-                          Choose Your Destination
-                        </label>
-                        <select
-                          name="Destination"
-                          className="form-select"
-                          aria-label="Default select example"
-                          id="chooseCategory"
-                          required
-                          value={destination}
-                          onChange={(e) => setDestination(e.target.value)}
-                        >
-                          <option value="">Select Destination</option>
-                          {tours.map((option, index) => (
-                            <option value={option.name} key={index}>
-                              {option.name}
-                            </option>
-                          ))}
-                        </select>
-                      </fieldset>
-                    </div>
-                    <div className="col-lg-12">
-                      <fieldset>
-                        <button className="main-button" type="submit">
-                          Make Your Reservation Now
-                        </button>
-                      </fieldset>
-                    </div>
-                  </div>
-                </form>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+
       {reservationSuccess && (
         <div className="reservation-success">
-          <div className="reservation-success-content">
-            <h3>Reservation Successful!</h3>
-            <p>Thank you for making your reservation. Here are the details:</p>
-            {reservationData && (
-              <>
-                <p>
-                  <strong>Name:</strong> {reservationData.name}
-                </p>
-                <p>
-                  <strong>Number:</strong> {reservationData.number}
-                </p>
-                <p>
-                  <strong>Number Of Guests:</strong> {reservationData.guests}
-                </p>
-                <p>
-                  <strong>Check In Date:</strong> {reservationData.checkInDate}
-                </p>
-                <p>
-                  <strong>Destination:</strong> {reservationData.destination}
-                </p>
-              </>
-            )}
-            <button onClick={closeSuccessMessage}>Done</button>
+          <div className="reservation-success-message">
+            <h2>Reservation Successful!</h2>
+            <p>
+              Thank you, <span>{reservationData.name}</span>, for making a
+              reservation with us. We look forward to welcoming you and your{" "}
+              <span>{reservationData.guests}</span> guests to{" "}
+              <span>{reservationData.destination}</span> on{" "}
+              <span>{reservationData.checkInDate}</span>.
+            </p>
+            <button className="close-button" onClick={closeSuccessMessage}>
+              Done
+            </button>
           </div>
         </div>
       )}
-
-
     </>
   );
 }
--- src/components/reservation/SecondpageHeading.js
+++ src/components/reservation/SecondpageHeading.js
@@ -3,18 +3,7 @@ import React from 'react'
 function SecondpageHeading() {
   return (
     <>
-        <div className="second-page-heading">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-12">
-          <h4>Book Prefered Deal Here</h4>
-          <h2>Make Your Reservation</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt uttersi labore et dolore magna aliqua is ipsum suspendisse ultrices gravida</p>
-          <div className="main-button"><a href="about.html">Discover More</a></div>
-        </div>
-      </div>
-    </div>
-  </div>
+
     </>
   )
 }
