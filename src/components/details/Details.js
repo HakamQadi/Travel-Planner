2a8dfb0a6bd08a84292dd9aa04d561aa93a9d573
mohammed saeed <mohammed.f.zain@gmail.com>
2023-06-21 16:50:31 +0300

    done

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -1,42 +1,46 @@
-import React from 'react'
-import { tours } from '../../data/Data'
-import { Link, useParams } from 'react-router-dom'
-import './Details.css'
+import React from "react";
+import { tours } from "../../data/Data";
+import { Link, useParams } from "react-router-dom";
+import "./Details.css";
 
 const Details = () => {
-    const { id } = useParams();
-    const watch = tours.find((info) => info.id === Number(id));
+  const { id } = useParams();
+  const watch = tours.find((info) => info.id === Number(id));
 
-
-    return (
-        <div className='container amazing-deals '
-            style={{
-                width: '50rem',
-                
-            }}>
-            <div className="col-lg-12 col-sm-12 ">
-                <div className="item">
-                    <div className="row">
-                        <div className="col-lg-6">
-                            <div className="image">
-                                <img style={{ height: '25rem', objectFit: 'cover' }} src={watch.img} alt="" />
-                            </div>
-                        </div>
-                        <div className="col-lg-6 align-self-center">
-                            <div className="content">
-                                <h4 className='amazing'>{watch.name}</h4>
-                                <p>{watch.description}</p>
-                                <div className="main-button">
-                                    <Link to="/reservation">Make a Reservation</Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div
+      className="container amazing-deals "
+      style={{
+        width: "50rem",
+        marginBottom: "107px",
+      }}
+    >
+      <div className="col-lg-12 col-sm-12 ">
+        <div className="item">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="image">
+                <img
+                  style={{ height: "25rem", objectFit: "cover" }}
+                  src={watch.img}
+                  alt=""
+                />
+              </div>
             </div>
+            <div className="col-lg-6 align-self-center">
+              <div className="content">
+                <h4 className="amazing">{watch.name}</h4>
+                <p>{watch.description}</p>
+                <div className="main-button">
+                  <Link to="/reservation">Make a Reservation</Link>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
-
-export default Details
+export default Details;
